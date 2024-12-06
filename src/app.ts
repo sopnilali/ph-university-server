e5eab67fb16902c1ae0398dc279f62dd2b1c0ba0
Md Abdul Adud <sopnilali24@gmail.com>
2024-12-06 14:56:07 +0600

    Done part2

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use('/api/v1', router);
+
+const test = (req: Request, res: Response) => {
+  const a = 200;
+  res.send({
+    success: true,
+    message: 'API is working fine',
+    data: a,
+  });
+};
+
+app.get('/', test);
+
+app.use(globalErrorHandler);
+
+//Not Found
+app.use(notFound);
+
+export default app;
